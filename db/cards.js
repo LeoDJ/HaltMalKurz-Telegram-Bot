da5a14339dea4f01c3cfefa98a6a03d702c3f614
LeoDJ <leandro.spaeth@gmail.com>
2017-04-13 15:43:21 +0200

    add card stickers

---
+++ db/cards.js
@@ -0,0 +1,55 @@
+// The stickes used here were taken from https://github.com/Olfi01/HaltMalKurzBot
+module.exports = {
+    witzig: {
+        achMeinDein: {
+            kaenguru: 'BQADAgAD4QADuNXvD7DVvOPPLJ9FAg',
+            kleinkuenstler: 'BQADAgAD4wADuNXvD0s8uqEC4GaoAg',
+            pinguin: 'BQADAgAD5QADuNXvD4d55YERfBxOAg'
+        },
+        schnickSchnackSchnuck: {
+            kaenguru: 'BQADAgADDwEAArjV7w9XEYFbZCvsxwI',
+            kleinkuenstler: 'BQADAgADEQEAArjV7w_BxM73LDagLgI',
+            pinguin: 'BQADAgADEwEAArjV7w_IHY6ObhVJqgI'
+        },
+        gruppenSchnickSchnackSchnuck: {
+            kaenguru: 'BQADAgAD5wADuNXvD92soH-U6_CdAg',
+            kleinkuenstler: 'BQADAgAD6QADuNXvD5CXgSytenphAg',
+            pinguin: 'BQADAgAD6wADuNXvD2NaohEJcbgLAg'
+        },
+        haltMalKurz: {
+            kaenguru: 'BQADAgAD7QADuNXvD9AZy4cPVhiKAg',
+            kleinkuenstler: 'BQADAgAD7wADuNXvDyjwPV3Kw965Ag',
+            pinguin: 'BQADAgAD8QADuNXvDwRzCZZI2Yt2Ag'
+        },
+        notToDoListe: {
+            kaenguru: 'BQADAgADAQEAArjV7w9TBJCNyVVwugI',
+            kleinkuenstler: 'BQADAgADAwEAArjV7w_9XHpoMREvtQI',
+            pinguin: 'BQADAgADBQEAArjV7w-TOOnmjYoSgAI'
+        },
+        vollVersammlung: {
+            kaenguru: 'BQADAgADFQEAArjV7w-xyus4DoA1tgI',
+            kleinkuenstler: 'BQADAgADFwEAArjV7w_UmAHHSysVCAI',
+            pinguin: 'BQADAgADGQEAArjV7w-ZcGk7g_ZyTQI'
+        },
+        kommunismus: 'BQADAgAD-QADuNXvDybdi8lkuQZQAg'
+    },
+    nichtWitzig: {
+        kapitalismus: {
+            kaenguru: 'BQADAgAD8wADuNXvD0AfzlnAnDcBAg',
+            kleinkuenstler: 'BQADAgAD9QADuNXvD0ThX6to2JdqAg',
+            pinguin: 'BQADAgAD9wADuNXvDzw_kIVSZQ1TAg'
+        },
+        nazi: { //[style 1, style 2] style 2 cards not implemented yet
+            kaenguru: ['BQADAgAD-wADuNXvD3Q3fLfoyHO8Ag'],
+            kleinkuenstler: ['BQADAgAD_QADuNXvD3ZMEwHbIey6Ag'],
+            pinguin: ['BQADAgAD_wADuNXvD95PjnX34SFmAg']
+
+        },
+        polizei: {
+            kaenguru: ['BQADAgADBwEAArjV7w8jLlfop-K6DgI'],
+            kleinkuenstler: ['BQADAgADCQEAArjV7w9Bmy1WHQlbLQI'],
+            pinguin: ['BQADAgADCwEAArjV7w8_OvK8YGuSKgI']
+        }
+    },
+    razupaltuff: 'BQADAgADDQEAArjV7w9c3w18rW2kIgI'
+};
